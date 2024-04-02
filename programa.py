03102de45040a20763f14c35ab1002a9d04570fd
Ismael Robles <56181706+iarobles-cursos@users.noreply.github.com>
2024-04-01 23:30:14 -0600

    Programa básico de ejemplo

---
+++ programa.py
@@ -0,0 +1,9 @@
+# Comentario python
+# Comentario 2 python
+arreglo = [7,1,35,42,1,4]
+num_a_buscar = 42
+for i in range(0,len(arreglo)):  # otro comentario
+    if num_a_buscar == arreglo[i]:
+        print("Encontrado")
+print("No encontrado")
+# Fin programa
